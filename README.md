c939e1f8e5761c253965dfd3e6d3874d0860897d
Aleks <aleksandar_atanasov91@abv.bg>
2016-04-21 01:21:35 +0200

    Initial settings & setting up the local server with hot module loader

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an application implementing a kanban with the help of Juho Vepsäläinen's book survivejs.
+More information comming soon.
---
+++ app/component.js
@@ -0,0 +1,7 @@
+module.exports = function ()  {
+  var element = document.createElement('h1');
+
+  element.innerHTML = "Hello responsive world!!!";
+
+  return element;
+};
